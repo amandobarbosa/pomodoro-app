3d77dc4bd9ac04b5464958cd681fd532836e4cce
amandobarbosa <amandobarbosa7@gmail.com>
2025-04-03 21:09:20 -0300

    add buttons

--- src/component/pomodoro-timer.tsx
+++ src/component/pomodoro-timer.tsx
@@ -1,4 +1,4 @@
-import { JSX, useState } from "react";
+import { JSX, useEffect, useState } from "react";
 import { useInterval } from "../hooks/use-setInterval";
 import { SecondsToTime } from "../utils/seconds-to-time";
 import { Button } from "./button";
@@ -12,18 +12,55 @@ interface Props {
 
 export function PomodoroTimer(props: Props): JSX.Element {
   const [mainTime, setMainTime] = useState(props.pomodoroTime);
-  useInterval(() => {
-    setMainTime(mainTime - 1);
-  }, 1000);
+  const [timeCounting, setTimeCount] = useState(false);
+  const [working, setWorking] = useState(false);
+  const [resting, setResting] = useState(false);
+  useInterval(
+    () => {
+      setMainTime(mainTime - 1);
+    },
+    timeCounting ? 1000 : null
+  );
+
+  const configureWork = () => {
+    setTimeCount(true);
+    setWorking(true);
+    setResting(false);
+    setMainTime(props.pomodoroTime);
+  };
+  const configureRest = (long: boolean) => {
+    setTimeCount(false);
+    setWorking(false);
+    setResting(true);
+
+    if (long) {
+      setMainTime(props.LongRestTime);
+    } else {
+      setMainTime(props.shortRestTime);
+    }
+  };
+  useEffect(() => {
+    if (working) {
+      document.body.setAttribute("class", "working");
+    }
+    if (resting) {
+      document.body.classList.remove("working");
+    }
+  }, [working]);
+
   return (
     <div className="pomodoro">
       <h2>You are: working</h2>
       <Timer mainTime={mainTime} />
 
       <div className="controls">
-        <Button onCLick={() => console.log(1)} text="test" />
-        <Button onCLick={() => console.log(1)} text="test" />
-        <Button onCLick={() => console.log(1)} text="test" />
+        <Button onCLick={configureWork} text="Work" />
+        <Button onCLick={() => configureRest(false)} text="Rest" />
+        <Button
+          className={!working && !resting ? "hidden" : ""}
+          onCLick={() => setTimeCount(!timeCounting)}
+          text={timeCounting ? "Pause" : "Play"}
+        />
       </div>
       <div className="details">
         <p>
